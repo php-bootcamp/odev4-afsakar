8d506c2fce620147a5338266df2137bbc712f785
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2021-02-19 20:40:56 +0300

    Azad Furkan ŞAKAR - Ödev4

--- SQL.php
+++ SQL.php
@@ -1,3 +1,23 @@
 <?php
 
 
+abstract class SQL {
+
+  use PDOConnector;
+
+  public $connectionStatus = false;
+
+  public abstract function create(SQLQuery $query);
+  public abstract function update(SQLQuery $query);
+  public abstract function get(SQLQuery $query);
+  public abstract function delete(SQLQuery $query);
+
+  public function setParams(array $params){
+
+  }
+
+  public function isConnected() {
+
+  }
+
+}
